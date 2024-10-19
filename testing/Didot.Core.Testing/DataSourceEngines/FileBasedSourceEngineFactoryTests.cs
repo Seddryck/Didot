0cceed2df4f0c0c05cc2b728989dc193210f498a
Cédric L. Charlier <seddryck@gmail.com>
2024-10-19 14:03:00 +0200

    test: improve code coverage

---
+++ testing/Didot.Core.Testing/DataSourceEngines/FileBasedSourceEngineFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Didot.Core.DataSourceEngines;
+using NUnit.Framework;
+
+namespace Didot.Core.Testing.DataSourceEngines;
+public class FileBasedSourceEngineFactoryTests
+{
+    [Test]
+    [TestCase(".json", typeof(JsonSource))]
+    [TestCase(".yaml", typeof(YamlSource))]
+    [TestCase(".yml", typeof(YamlSource))]
+    public void GetSourceParser_Extension_CorrectParser(string extension, Type expected)
+    {
+        var factory = new FileBasedSourceEngineFactory();
+        var parser = factory.GetSourceParser(extension);
+        Assert.That(parser, Is.TypeOf(expected));
+    }
+
+    [Test]
+    [TestCase(".toml")]
+    public void GetSourceParser_NotSupportedExtension_Exception(string extension)
+    {
+        var factory = new FileBasedSourceEngineFactory();
+        Assert.Throws<NotSupportedException>(() => factory.GetSourceParser(extension));
+    }
+}
