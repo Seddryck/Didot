55745b66bb961dfe29e8465382d66eeb0886eefd
Cédric L. Charlier <seddryck@gmail.com>
2024-10-19 11:22:45 +0200

    feat: json source parser

---
+++ src/Didot.Core/DataSourceEngines/JsonSource.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization;
+using System.Text.Json;
+using YamlDotNet.Core.Tokens;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Didot.Core.DataSourceEngines;
+public class JsonSource : ISourceParser
+{
+    public object Parse(string content)
+        => ParseToken(JToken.Parse(content))!;
+
+    private static object? ParseToken(JToken token)
+    {
+        if (token is JArray jArray)
+        {
+            var list = new List<object?>();
+            foreach (var item in jArray)
+                list.Add(ParseToken(item));
+            return list;
+        }
+        else if (token is JObject jObject)
+        {
+            var dict = new Dictionary<string, object?>();
+            foreach (var prop in jObject.Properties())
+                dict[prop.Name] = ParseToken(prop.Value);
+            return dict;
+        }
+        else if (token is JValue jValue)
+            return jValue.Value;
+
+        throw new NotSupportedException();
+    }
+
+    public object Parse(Stream stream)
+    {
+        using (var streamReader = new StreamReader(stream))
+        using (var jsonReader = new JsonTextReader(streamReader))
+            return ParseToken(JToken.Load(jsonReader))!;
+    }
+
+    private static Dictionary<string, object> Parse(JsonElement element)
+    {
+        if (element.ValueKind == JsonValueKind.Object)
+        {
+            var dict = new Dictionary<string, object>();
+            foreach (var property in element.EnumerateObject())
+            {
+                dict[property.Name] = Parse(property.Value);
+            }
+            return dict;
+        }
+        else
+            throw new NotSupportedException();
+    }
+}
---
+++ testing/Didot.Core.Testing/DataSourceEngines/JsonSourceTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Didot.Core.DataSourceEngines;
+using NUnit.Framework;
+
+namespace Didot.Core.Testing.DataSourceEngines;
+public class JsonSourceTests
+{
+    [Test]
+    public void Parse_SingleProperty_Successful()
+    {
+        var source = new JsonSource();
+        dynamic result = source.Parse("{\"Name\": \"World\"}");
+        Assert.That(result, Is.AssignableTo<IDictionary<string, object>>());
+        Assert.That(result, Does.ContainKey("Name"));
+        Assert.That(result["Name"], Is.EqualTo("World"));
+    }
+
+    [Test]
+    public void Parse_MultipleProperties_Successful()
+    {
+        var source = new JsonSource();
+        dynamic result = source.Parse("{\"Name\": \"Albert\", \"Age\": 30}");
+        Assert.That(result, Is.AssignableTo<IDictionary<string, object>>());
+        Assert.That(result, Does.ContainKey("Name"));
+        Assert.That(result["Name"], Is.EqualTo("Albert"));
+        Assert.That(result, Does.ContainKey("Age"));
+        Assert.That(result["Age"], Is.EqualTo(30));
+    }
+
+    [Test]
+    public void Parse_NestedProperties_Successful()
+    {
+        var source = new JsonSource();
+        dynamic result = source.Parse("{\"Name\":\r\n  {\"First\": \"Albert\",\r\n  \"Last\": \"Einstein\"\r\n}, \"Age\": 30}");
+        Assert.That(result, Is.AssignableTo<IDictionary<string, object>>());
+        Assert.That(result, Does.ContainKey("Name"));
+        Assert.That(result["Name"], Is.AssignableTo<IDictionary<string, object>>());
+        Assert.That(result["Name"], Does.ContainKey("First"));
+        Assert.That(result["Name"]["First"], Is.EqualTo("Albert"));
+        Assert.That(result["Name"], Does.ContainKey("Last"));
+        Assert.That(result["Name"]["Last"], Is.EqualTo("Einstein"));
+        Assert.That(result, Does.ContainKey("Age"));
+        Assert.That(result["Age"], Is.EqualTo(30));
+    }
+
+    [Test]
+    public void Parse_Array_Successful()
+    {
+        var source = new JsonSource();
+        dynamic result = source.Parse("[{\"Name\": \"Albert\",\r\n  \"Age\": 30},\r\n{\"Name\": \"Nikola\",\r\n  \"Age\": 50}]");
+        Assert.That(result, Is.AssignableTo<List<object>>());
+        Assert.That(result[0], Does.ContainKey("Name"));
+        Assert.That(result[0]["Name"], Is.EqualTo("Albert"));
+        Assert.That(result[0], Does.ContainKey("Age"));
+        Assert.That(result[0]["Age"], Is.EqualTo(30));
+    }
+
+    [Test]
+    public void Parse_Stream_Successful()
+    {
+        var source = new JsonSource();
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes("{\"Name\": \"World\"}"));
+        dynamic result = source.Parse(stream);
+        Assert.That(result, Is.AssignableTo<IDictionary<string, object>>());
+        Assert.That(result, Does.ContainKey("Name"));
+        Assert.That(result["Name"], Is.EqualTo("World"));
+    }
+}
