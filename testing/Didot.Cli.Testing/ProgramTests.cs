6c348602dce8d023726c3d1b1bbe19006c678633
Cédric L. Charlier <seddryck@gmail.com>
2024-10-20 09:50:18 +0200

    feat: output redirected to StandardOut

--- testing/Didot.Cli.Testing/ProgramTests.cs
+++ testing/Didot.Cli.Testing/ProgramTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,31 +11,58 @@ namespace Didot.Cli.Testing;
 
 public class ProgramTests
 {
-    private static string[] Templates = { "scriban", "liquid", "hbs", "smart" };
-    public static string[] DataSets = { "yaml", "json", "xml" };
+    private static readonly string[] Templates = { "scriban", "liquid", "hbs", "smart" };
+    private static readonly string[] DataSets = { "yaml", "json", "xml" };
+
+    private TextWriter OriginalOutput { get; set; }
+    private Stream MemoryStream { get; set; }
+    private StreamWriter Writer { get; set; }
+
+    [SetUp]
+    public void SetUp()
+    {
+        OriginalOutput = Console.Out;
+        MemoryStream = new MemoryStream();
+        Writer = new(MemoryStream)
+        {
+            AutoFlush = true
+        };
+        Console.SetOut(Writer);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Writer.Dispose();
+        MemoryStream.Dispose();
+        Console.SetOut(OriginalOutput);
+    }
 
     [Test, Combinatorial]
-    public void Main_WithNoArguments_ShouldPrintHelp(
-        [ValueSource(nameof(Templates))] string engine,
-        [ValueSource(nameof(DataSets))] string data)
+    public void CaptureConsoleOutputTest(
+            [ValueSource(nameof(Templates))] string engine,
+            [ValueSource(nameof(DataSets))] string data)
     {
         var args = new string[]
         {
-            $"-ttemplate/template-01.{engine}",
-            $"-sdata/data-01.{data}",
-            $"-ooutput-01-{engine}-{data}.txt"
+                    $"-ttemplate/template-01.{engine}",
+                    $"-sdata/data-01.{data}"
         };
         Program.Main(args);
 
-        var output = File.ReadAllText($"output-01-{engine}-{data}.txt").Uniform();
-        var expected = File.ReadAllText($"Expectation/expectation-01.txt").Uniform();
-        Assert.That(output, Is.EqualTo(expected));
+        MemoryStream.Position = 0;
+        using (var reader = new StreamReader(MemoryStream))
+        {
+            var consoleOutput = reader.ReadToEnd().Standardize();
+            var expected = File.ReadAllText($"Expectation/expectation-01.txt").Standardize();
+            Assert.That(consoleOutput, Is.EqualTo(expected));
+        }
     }
 }
 
 public static class StringExtensions
 {
-    public static string Uniform(this string value)
+    public static string Standardize(this string value)
     {
         var result = value.Trim().Replace("\r\n", "\n").Trim();
         return result;
