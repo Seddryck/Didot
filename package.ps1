7c91745f12986f180c79d1d27a728ba8768b586d
Cédric L. Charlier <seddryck@gmail.com>
2024-10-21 18:24:22 +0200

    fix: prevent self-contained

--- package.ps1
+++ package.ps1
@@ -16,7 +16,7 @@ foreach ($project in $projects) {
             dotnet build $project -p:version="$env:GitVersion_SemVer" -c Release -f $framework -r $runtime /p:ContinuousIntegrationBuild=true --nologo
             if ($project -like "*Didot.Cli*") {
                 Write-Host "Packaging .exe artifacts $project for $framework on $runtime..."
-                dotnet publish $project -c Release -f $framework -r $runtime -o ./.publish/$framework/$runtime --no-build --nologo
+                dotnet publish $project -c Release -f $framework -r $runtime --no-self-contained -o ./.publish/$framework/$runtime --no-build --nologo
                 7z a ./.publish/Didot-$env:GitVersion_SemVer-$framework-$runtime.zip ./.publish/$framework/$runtime/*.*
             }
         }
