060295f294f018509c55d9f6ffd35ff70d6345f4
Cédric L. Charlier <seddryck@gmail.com>
2024-10-29 23:28:58 +0100

    feat: availability of helpers  for Handlebars (#40)

--- src/Didot.Core/TemplateEngines/HandlebarsWrapper.cs
+++ src/Didot.Core/TemplateEngines/HandlebarsWrapper.cs
@@ -5,20 +5,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HandlebarsDotNet;
+using HandlebarsDotNet.Helpers;
+using HandlebarsDotNet.Helpers.Enums;
 
 namespace Didot.Core.TemplateEngines;
 public class HandlebarsWrapper : ITemplateEngine
 {
     public string Render(string template, object model)
     {
-        var templateInstance = Handlebars.Compile(template);
+        var handlebarsContext = CreateContext();
+        var templateInstance = handlebarsContext.Compile(template);
         return templateInstance(model);
     }
 
     public string Render(Stream stream, object model)
     {
+        var handlebarsContext = CreateContext();
+
         using var reader = new StreamReader(stream);
-        var template = reader.ReadToEnd();
-        return Render(template, model);
+        var templateInstance = handlebarsContext.Compile(reader);
+
+        using var writer = new StringWriter(); // StringWriter as TextWriter for output
+        templateInstance(writer, model);
+        return writer.ToString();
+    }
+
+    private IHandlebars CreateContext()
+    {
+        var handlebarsContext = Handlebars.Create();
+        HandlebarsHelpers.Register(handlebarsContext);
+        return handlebarsContext;
     }
 }
