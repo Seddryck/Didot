852058ff20072a39b5731fff67340478a81faa91
Cédric L. Charlier <seddryck@gmail.com>
2025-05-03 14:43:29 +0200

    feat: template engine factory accepting builders

---
+++ src/Didot.Core/TemplateEngineFactory.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Didot.Core;
+public class TemplateEngineFactory
+{
+    private Dictionary<string, ITemplateEngineBuildable> _engines = [];
+
+    public TemplateEngineFactory()
+    { }
+
+    private static TemplateEngineFactory BuildDefault()
+    {
+        var factory = new TemplateEngineFactory();
+        factory.AddOrReplace(".liquid", new TemplateEngineBuilder().UseDotLiquid());
+        factory.AddOrReplace(".hbs", new TemplateEngineBuilder().UseHandlebars());
+        factory.AddOrReplace(".stg", new TemplateEngineBuilder().UseStringTemplate());
+        factory.AddOrReplace(".st", new TemplateEngineBuilder().UseStringTemplate());
+        factory.AddOrReplace(".morestachio", new TemplateEngineBuilder().UseMorestachio());
+        factory.AddOrReplace(".mustache", new TemplateEngineBuilder().UseMorestachio());
+        factory.AddOrReplace(".smart", new TemplateEngineBuilder().UseSmartFormat());
+        factory.AddOrReplace(".scriban", new TemplateEngineBuilder().UseScriban());
+        return factory;
+    }
+    private static TemplateEngineFactory _default = BuildDefault();
+    public static TemplateEngineFactory Default { get; } = _default;
+
+    public void Clear()
+        => _engines.Clear();
+
+    public void Remove(string extension)
+    {
+        if (string.IsNullOrEmpty(extension))
+            throw new ArgumentNullException(nameof(extension));
+        if (!extension.StartsWith('.'))
+            extension = '.' + extension;
+        _engines.Remove(extension);
+    }
+
+    public bool Exists(string extension)
+    {
+        if (string.IsNullOrEmpty(extension))
+            throw new ArgumentNullException(nameof(extension));
+        if (!extension.StartsWith('.'))
+            extension = '.' + extension;
+        return _engines.ContainsKey(extension);
+    }
+
+    public int Count => _engines.Count;
+
+    public void AddOrReplace(string extension, ITemplateEngineBuildable engine)
+    {
+        if (string.IsNullOrEmpty(extension))
+            throw new ArgumentNullException(nameof(extension));
+
+        if (!extension.StartsWith('.'))
+            extension = '.' + extension;
+
+        if (!_engines.TryAdd(extension, engine))
+            _engines[extension] = engine;
+    }
+
+    public void AddOrReplace(string extension, Func<TemplateEngineBuilder, ITemplateEngineBuildable> engine)
+        => AddOrReplace(extension, engine(new ()));
+
+    public string[] AllSupportedExtensions()
+        => [.. _engines.Keys];
+
+    public ITemplateEngine Create(string extension)
+    {
+        if (string.IsNullOrEmpty(extension))
+            throw new ArgumentNullException(nameof(extension));
+
+        if (!extension.StartsWith('.'))
+            extension = '.' + extension;
+
+        if (_engines.TryGetValue(extension, out var engine))
+            return engine.Build();
+        throw new NotSupportedException($"Template engine for extension '{extension}' is not supported.");
+    }
+}
---
+++ testing/Didot.Core.Testing/TemplateEngineFactoryTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Didot.Core.TemplateEngines;
+using HandlebarsDotNet;
+using NUnit.Framework;
+
+namespace Didot.Core.Testing;
+public class TemplateEngineFactoryTests
+{
+    [Test]
+    [TestCase(".liquid", typeof(DotLiquidWrapper))]
+    [TestCase(".hbs", typeof(HandlebarsWrapper))]
+    [TestCase(".stg", typeof(StringTemplateWrapper))]
+    [TestCase(".st", typeof(StringTemplateWrapper))]
+    [TestCase(".morestachio", typeof(MorestachioWrapper))]
+    [TestCase(".mustache", typeof(MorestachioWrapper))]
+    [TestCase(".smart", typeof(SmartFormatWrapper))]
+    [TestCase(".scriban", typeof(ScribanWrapper))]
+    public void Create_Default_Expected(string extension, Type expected)
+    {
+        var factory = TemplateEngineFactory.Default;
+        var engine = factory.Create(extension);
+        Assert.That(engine, Is.TypeOf(expected));
+    }
+
+    [Test]
+    public void Create_Custom_Expected()
+    {
+        var factory = new TemplateEngineFactory();
+        factory.AddOrReplace(".st", engine => engine.UseStringTemplate(opt => opt.WithDollarDelimitedExpressions()));
+        var engine = factory.Create(".st");
+        Assert.That(engine, Is.TypeOf<StringTemplateWrapper>());
+        var stWrapper = (StringTemplateWrapper)engine;
+        Assert.That(stWrapper.Options.Delimiters.Left, Is.EqualTo('$'));
+        Assert.That(stWrapper.Options.Delimiters.Right, Is.EqualTo('$'));
+    }
+
+    [Test]
+    public void ClearRemoveExists_Custom_Expected()
+    {
+        var factory = new TemplateEngineFactory();
+        factory.AddOrReplace(".st", engine => engine.UseStringTemplate(opt => opt.WithDollarDelimitedExpressions()));
+        factory.AddOrReplace(".scriban", engine => engine.UseScriban());
+        Assert.That(factory.AllSupportedExtensions(), Has.Length.EqualTo(2));
+
+        factory.Remove(".scriban");
+        Assert.That(factory.AllSupportedExtensions(), Has.Length.EqualTo(1));
+        Assert.That(factory.AllSupportedExtensions(), Has.Member(".st"));
+        Assert.That(factory.Exists(".scriban"), Is.False);
+        Assert.That(factory.Exists(".st"), Is.True);
+
+        factory.Clear();
+        Assert.That(factory.Count, Is.EqualTo(0));
+        Assert.That(factory.Exists(".st"), Is.False);
+    }
+}
