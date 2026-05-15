2bd1635272a01f6b4f6fba9a758c64fbdf6b2bc4
Cédric L. Charlier <seddryck@gmail.com>
2026-05-15 12:54:24 +0200

    feat: provide a Windows Installer for Didot

--- package.ps1
+++ package.ps1
@@ -2,25 +2,36 @@ if (Test-Path "./.publish") {
     Remove-Item -Recurse -Force "./.publish"
 }
 
+$env:SuppressNETCoreSdkPreviewMessage = "true"
+
+$version = $env:GitVersion_SemVer
+if ([string]::IsNullOrWhiteSpace($version)) {
+    $version = "0.0.0-local"
+}
+
 $projects = @(
   "src/Didot.Core/Didot.Core.csproj",
   "src/Didot.Cli/Didot.Cli.csproj"
 )
-$frameworks = @("net8.0", "net9.0")
+$frameworks = @("net8.0", "net9.0", "net10.0")
 $runtimes = ("win-x64", "linux-x64")
 
 foreach ($project in $projects) {
     foreach ($framework in $frameworks) {
-        foreach ($runtime in $runtimes) {
-            Write-Host "Building $project for $framework on $runtime ..."
-            dotnet build $project -p:version="$env:GitVersion_SemVer" -c Release -f $framework -r $runtime /p:ContinuousIntegrationBuild=true --nologo
-            if ($project -like "*Didot.Cli*") {
+        if ($project -like "*Didot.Core*") {
+            dotnet build $project -p:version="$version" -c Release -f $framework /p:ContinuousIntegrationBuild=true --nologo
+        } else {
+            foreach ($runtime in $runtimes) {
+                Write-Host "Building $project for $framework on $runtime ..."
+                dotnet build $project -p:version="$version" -c Release -f $framework -r $runtime /p:ContinuousIntegrationBuild=true --nologo
                 Write-Host "Packaging .exe artifacts $project for $framework on $runtime..."
-                dotnet publish $project -c Release -f $framework -r $runtime --no-self-contained -o ./.publish/$framework/$runtime --no-build --nologo
-                7z a ./.publish/Didot-$env:GitVersion_SemVer-$framework-$runtime.zip ./.publish/$framework/$runtime/*.*
+                dotnet publish $project -p:version="$version" -c Release -f $framework -r $runtime --no-self-contained -o ./.publish/$framework/$runtime --no-build --nologo
+                7z a ./.publish/Didot-$version-$framework-$runtime.zip ./.publish/$framework/$runtime/*.*                
             }
         }
     }
     Write-Host "Packaging Nuget $project ..."
-    dotnet pack $project -p:version="$env:GitVersion_SemVer" -c Release --include-symbols --no-build --nologo
-}    
+    dotnet pack $project -p:version="$version" -c Release --include-symbols --no-build --nologo
+}
+Write-Host "Building Windows installer for Didot CLI ..."
+iscc "/DAppVersion=$version" "/DTargetFramework=net10.0" "/DRuntimeIdentifier=win-x64" ".\distribution\installer\didot-cli.iss"
